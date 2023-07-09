ee430d59f3f92d40d424eaf48381cf290db6b87c
amienyahya <amien.yahya5@gmail.com>
2023-07-09 07:16:24 +0700

    Persiapanproject

---
+++ twitter_clone/lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '64732c84e8877af0c7ed';
+  static const String projectId = '642933456c45665c5061';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
